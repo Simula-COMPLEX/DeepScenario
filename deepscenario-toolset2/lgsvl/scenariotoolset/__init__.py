d8c2ab135e721074d359392539365bef90109e11
chengjie-lu <chengjielu@simula.no>
2023-01-26 00:14:10 +0100

    update deepscenario toolset

--- deepscenario-toolset2/lgsvl/scenariotoolset/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 17/01/2023 16:00
-# @Author  : Chengjie
-# @File    : __init__.py.py
-# @Software: PyCharm
-
-from .scenario_runner import ScenarioRunner
-from .scenario_collector import ScenarioCollector
--- deepscenario-toolset2/scripts/run_and_create_coverage_report.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-~/.local/bin/coverage run -m unittest discover -v -c
-~/.local/bin/coverage html --omit "~/.local/*","tests/*"
